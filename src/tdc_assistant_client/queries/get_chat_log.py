0f3d174874c249226a07fd268d872624764f420c
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-23 17:14:45 -0500

    Add code editor mutations

--- src/tdc_assistant_client/queries/get_chat_log.py
+++ src/tdc_assistant_client/queries/get_chat_log.py
@@ -2,80 +2,94 @@ from gql import gql
 
 get_chat_log_query = gql(
     """
-    query ChatLog($chatLogId: ID!) {
-      chatLog(id: $chatLogId) {
+query ChatLog($chatLogId: ID!) {
+  chatLog(id: $chatLogId) {
+    createdAt
+    customerName
+    deletedAt
+    id
+    messages {
+      id
+      createdAt
+      updatedAt
+      deletedAt
+      content
+      role
+      annotations {
+        ... on ResponseAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          type
+          content
+          messageId
+        }
+        ... on WorkspaceAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          messageId
+          type
+          content
+          boardNumber
+        }
+        ... on ChatCompletionAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          sentAt
+          messageId
+          type
+          approvalStatus
+          responseAnnotationId
+          chatCompletion {
+            id
+            createdAt
+            updatedAt
+            deletedAt
+            parts {
+              id
+              createdAt
+              updatedAt
+              deletedAt
+              content
+              type
+              programmingLanguage
+              shouldOmit
+            }
+          }
+        }
+        ... on ImageCaptureAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          imageCapture {
+            id
+            createdAt
+            updatedAt
+            deletedAt
+            imageUrl
+          }
+        }
+      }
+    }
+    updatedAt
+    workspaces {
+      ... on CodeEditor {
         id
         createdAt
         updatedAt
         deletedAt
-        customerName
-        messages {
-          id
-          createdAt
-          updatedAt
-          deletedAt
-          content
-          role
-          annotations {
-            ... on ResponseAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              type
-              content
-              messageId
-            }
-            ... on WorkspaceAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              messageId
-              type
-              content
-              boardNumber
-            }
-            ... on ChatCompletionAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              sentAt
-              messageId
-              type
-              approvalStatus
-              responseAnnotationId
-              chatCompletion {
-                id
-                createdAt
-                updatedAt
-                deletedAt
-                parts {
-                  id
-                  createdAt
-                  updatedAt
-                  deletedAt
-                  content
-                  type
-                  programmingLanguage
-                  shouldOmit
-                }
-              }
-            }
-          }
-        }
-        workspaces {
-          id
-          createdAt
-          updatedAt
-          deletedAt
-          chatLogId
-          boardNumber
-          type
-          content
-        }
+        programmingLanguage
+        editorNumber
+        content
       }
     }
+  }
+}
 """
 )
--- src/tdc_assistant_client/queries/get_chat_logs.py
+++ src/tdc_assistant_client/queries/get_chat_logs.py
@@ -2,80 +2,94 @@ from gql import gql
 
 get_chat_logs_query = gql(
     """
-    query ChatLogs {
-      chatLogs {
+  query ChatLogs {
+  chatLogs {
+    id
+    createdAt
+    updatedAt
+    deletedAt
+    customerName
+    messages {
+      id
+      createdAt
+      updatedAt
+      deletedAt
+      content
+      role
+      annotations {
+        ... on ResponseAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          type
+          content
+          messageId
+        }
+        ... on WorkspaceAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          messageId
+          type
+          content
+          boardNumber
+        }
+        ... on ChatCompletionAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          sentAt
+          messageId
+          type
+          approvalStatus
+          responseAnnotationId
+          chatCompletion {
+            id
+            createdAt
+            updatedAt
+            deletedAt
+            parts {
+              id
+              createdAt
+              updatedAt
+              deletedAt
+              content
+              type
+              programmingLanguage
+              shouldOmit
+            }
+          }
+        }
+        ... on ImageCaptureAnnotation {
+          id
+          createdAt
+          updatedAt
+          deletedAt
+          imageCapture {
+            id
+            createdAt
+            updatedAt
+            deletedAt
+            imageUrl
+          }
+        }
+      }
+    }
+    workspaces {
+      ... on CodeEditor {
         id
         createdAt
         updatedAt
         deletedAt
-        customerName
-        messages {
-          id
-          createdAt
-          updatedAt
-          deletedAt
-          content
-          role
-          annotations {
-            ... on ResponseAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              type
-              content
-              messageId
-            }
-            ... on WorkspaceAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              messageId
-              type
-              content
-              boardNumber
-            }
-            ... on ChatCompletionAnnotation {
-              id
-              createdAt
-              updatedAt
-              deletedAt
-              sentAt
-              messageId
-              type
-              approvalStatus
-              responseAnnotationId
-              chatCompletion {
-                id
-                createdAt
-                updatedAt
-                deletedAt
-                parts {
-                  id
-                  createdAt
-                  updatedAt
-                  deletedAt
-                  content
-                  type
-                  programmingLanguage
-                  shouldOmit
-                }
-              }
-            }
-          }
-        }
-        workspaces {
-          id
-          createdAt
-          updatedAt
-          deletedAt
-          chatLogId
-          boardNumber
-          type
-          content
-        }
+        programmingLanguage
+        editorNumber
+        content
       }
     }
+  }
+}
 """
 )
