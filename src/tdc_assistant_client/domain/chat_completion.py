3360f727c2c522e27a7dbc72466487ec848288d9
Adam Carter <adam.clayton.carter@gmail.com>
2023-11-29 10:32:34 -0500

    Add chat completion approval mutation

--- src/tdc_assistant_client/domain/chat_completion.py
+++ src/tdc_assistant_client/domain/chat_completion.py
@@ -1,7 +1,9 @@
 from .base import Base
 from .chat_completion_part import ChatCompletionPart
+from .chat_completion_approval_status import *
 
 
 class ChatCompletion(Base):
     content: str
+    approvalStatus: ChatCompletionApprovalStatus
     parts: list[ChatCompletionPart]
